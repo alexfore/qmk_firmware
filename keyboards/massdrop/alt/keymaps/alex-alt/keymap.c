7958d475490d2380748d76a63ed867ee93728251
fore <alexander.fore@jpl.nasa.gov>
2020-09-14 16:17:45 -0400

    add mod-tap []{} to layers

--- keyboards/massdrop/alt/keymaps/alex-alt/keymap.c
+++ keyboards/massdrop/alt/keymaps/alex-alt/keymap.c
@@ -26,13 +26,19 @@ keymap_config_t keymap_config;
 
 //Tap Dance Declarations
 enum {
-  CTRL = 0
-};
-//Tap Dance Definitions
-qk_tap_dance_action_t tap_dance_actions[] = {
-  [CTRL] = ACTION_TAP_DANCE_DOUBLE(KC_LCTL, KC_CAPS),
+  LBC,
+  RBC,
 };
 
+//Tap Dance Definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [LBC] = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, KC_LCBR),
+  [RBC] = ACTION_TAP_DANCE_DOUBLE(KC_RBRC, KC_RCBR),
+};
+
+
+#define MT_LSO LSFT_T(KC_LBRC)
+#define MT_RSO RSFT_T(KC_RBRC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_65_ansi_blocker(
@@ -46,14 +52,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_MUTE, \
         _______, RGB_SPD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, _______, U_T_AUTO,U_T_AGCR,_______, KC_PSCR, KC_SLCK, KC_PAUS, _______, KC_END,  \
         _______, RGB_RMOD,RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, _______, _______, _______, _______, _______, _______,          _______, KC_VOLU, \
-        _______, RGB_TOG, _______, _______, _______, MD_BOOT, NK_TOGG, DBG_TOG, _______, _______, _______, _______,          KC_PGUP, KC_VOLD, \
+        TD(LBC), RGB_TOG, _______, _______, _______, MD_BOOT, NK_TOGG, DBG_TOG, _______, _______, _______, TD(RBC),          KC_PGUP, KC_VOLD, \
         _______, _______, _______,                            _______,                            _______, _______, KC_HOME, KC_PGDN, KC_END   \
     ),
     [_RAISE] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, KC_7,    KC_8,    KC_9,    KC_SLSH, _______, _______, _______, KC_MUTE, \
         _______, _______, _______, _______, _______, _______, _______, KC_4,    KC_5,    KC_6,    KC_ASTR, _______, _______, _______, KC_END,  \
         _______, _______, _______, _______, _______, _______, _______, KC_1,    KC_2,    KC_3,    KC_MINS, _______,          _______, KC_VOLU, \
-        _______, _______, _______, _______, _______, _______, _______, KC_0,    _______, _______, KC_PLUS, _______,          KC_PGUP, KC_VOLD, \
+        TD(LBC), _______, _______, _______, _______, _______, _______, KC_0,    _______, _______, KC_PLUS, TD(RBC),          KC_PGUP, KC_VOLD, \
         _______, _______, _______,                            _______,                            _______, _______, KC_HOME, KC_PGDN, KC_END   \
     ),
     [_ADJUST] = LAYOUT(
