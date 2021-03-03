f3c429e9d7eb9992ca00b666bf58c56eee5358ee
fore <alexander.fore@jpl.nasa.gov>
2021-03-03 14:38:13 -0500

    fix some stuff on layers, change colors, add led cycling modes

--- keyboards/massdrop/alt/keymaps/alex-alt/keymap.c
+++ keyboards/massdrop/alt/keymaps/alex-alt/keymap.c
@@ -49,14 +49,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         RAISE,   KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RGUI, LOWER,   KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
     [_LOWER] = LAYOUT_65_ansi_blocker(
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_MUTE, \
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_MUTE, \
         _______, RGB_SPD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, _______, U_T_AUTO,U_T_AGCR,_______, KC_PSCR, KC_MPRV, KC_MNXT, _______, KC_MPLY, \
         _______, RGB_RMOD,RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, _______, _______, _______, _______, _______, _______,          _______, KC_VOLU, \
         TD(LBC), RGB_TOG, _______, _______, _______, MD_BOOT, NK_TOGG, DBG_TOG, _______, _______, _______, TD(RBC),          KC_PGUP, KC_VOLD, \
         _______, _______, _______,                            _______,                            _______, _______, KC_HOME, KC_PGDN, KC_END   \
     ),
     [_RAISE] = LAYOUT(
-        _______, _______, _______, _______, _______, _______, _______, KC_7,    KC_8,    KC_9,    KC_SLSH, _______, _______, _______, KC_MUTE, \
+        _______, _______, _______, _______, _______, _______, _______, KC_7,    KC_8,    KC_9,    KC_SLSH, _______, _______, KC_DEL,  KC_MUTE, \
         _______, _______, _______, _______, _______, _______, _______, KC_4,    KC_5,    KC_6,    KC_ASTR, KC_MPRV, KC_MNXT, _______, KC_MPLY, \
         _______, _______, _______, _______, _______, _______, _______, KC_1,    KC_2,    KC_3,    KC_MINS, _______,          _______, KC_VOLU, \
         TD(LBC), _______, _______, _______, _______, _______, _______, KC_0,    _______, _______, KC_PLUS, TD(RBC),          KC_PGUP, KC_VOLD, \
@@ -76,10 +76,10 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     uint8_t layer = biton32(state);
     switch (biton32(state)) {
         case _QWERTY:
-            rgb_matrix_sethsv_noeeprom(213, 255, 150);
+            rgb_matrix_sethsv_noeeprom(100, 255, 110);
             break;
         case _LOWER:
-            rgb_matrix_sethsv_noeeprom(100, 255, 250);
+            rgb_matrix_sethsv_noeeprom(213, 255, 250);
             break;
         case _RAISE:
             rgb_matrix_sethsv_noeeprom(50, 255, 250);
@@ -97,9 +97,9 @@ void matrix_init_user(void) {
 };
 
 void keyboard_post_init_user(void) {
-    rgb_matrix_set_flags(LED_FLAG_KEYLIGHT);
+    rgb_matrix_set_flags(LED_FLAG_KEYLIGHT|LED_FLAG_MODIFIER);
     rgb_matrix_mode_noeeprom(1);
-    rgb_matrix_sethsv_noeeprom(213, 255, 150);
+    rgb_matrix_sethsv_noeeprom(100, 255, 110);
 };
 
 // Runs constantly in the background, in a loop.
@@ -181,11 +181,21 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             if (record->event.pressed) {
               switch (rgb_matrix_get_flags()) {
                 case LED_FLAG_ALL: {
+                    rgb_matrix_set_flags(LED_FLAG_KEYLIGHT|LED_FLAG_MODIFIER);
+                    rgb_matrix_set_color_all(0, 0, 0);
+                  }
+                  break;
+                case (LED_FLAG_KEYLIGHT|LED_FLAG_MODIFIER): {
                     rgb_matrix_set_flags(LED_FLAG_KEYLIGHT);
                     rgb_matrix_set_color_all(0, 0, 0);
                   }
                   break;
-                case LED_FLAG_KEYLIGHT: {
+                case (LED_FLAG_KEYLIGHT): {
+                    rgb_matrix_set_flags(LED_FLAG_MODIFIER);
+                    rgb_matrix_set_color_all(0, 0, 0);
+                  }
+                  break;
+                case (LED_FLAG_MODIFIER): {
                     rgb_matrix_set_flags(LED_FLAG_UNDERGLOW);
                     rgb_matrix_set_color_all(0, 0, 0);
                   }
